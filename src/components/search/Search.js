510f98431911e97753973a36fe71cc2ba96e764b
Bam Kadayat <bamkadayat@Bams-MacBook-Air.local>
2020-04-24 12:51:28 +0200

    imports some style function from material ui in imageResults file to render images in grid styles

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -3,6 +3,7 @@ import TextField from 'material-ui/TextField';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
 import axios from  'axios';
+import ImageResults from '../image-results/ImageResults';
 
 
 class Search extends Component {
@@ -25,7 +26,14 @@ class Search extends Component {
                 .catch(err =>console.log(err));
         });
     }
+
+    onAmountChange = (e, index, value) =>{
+        this.setState({
+            amount: value
+        })
+    }
     render() {
+                     
         return (
             <div>
                 <TextField 
@@ -50,6 +58,7 @@ class Search extends Component {
                     <MenuItem value={50} primaryText="50" />
                 </SelectField>
                 <br />
+                {this.state.images.length > 0 ? (<ImageResults images={this.state.images} />):null}
             </div>
         )
     }
